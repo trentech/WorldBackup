ad0228b3663a7b439cafb1db5e01464bc23d5000
TrenTech@gmail.com <TrenTech@gmail.com>
2016-06-10 10:50:49 -0400

    SpongeGradle

--- src/main/java/com/gmail/trentech/worldbackup/utils/Resource.java
+++ src/main/java/com/gmail/trentech/worldbackup/utils/Resource.java
@@ -3,10 +3,9 @@ package com.gmail.trentech.worldbackup.utils;
 public class Resource {
 
 	public final static String NAME = "World Backup";
-	public final static String VERSION = "0.3.5";
+	public final static String VERSION = "@project.version@";
 	public final static String ID = "com.gmail.trentech.worldbackup";
 	public final static String AUTHOR = "TrenTech";
 	public final static String URL = "http://github.com/TrenTech/WorldBackup";
-	public final static String DESCRIPTION = "World backup management";
 	
 }
